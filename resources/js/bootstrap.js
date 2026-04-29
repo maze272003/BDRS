44e2ccee8c5d75b3a88760bb570ad584e7054471
johnmichael-jonatas <johnmichael.jonatas@microsource.com.ph>
2026-04-30 00:13:09 +0800

    pusherjs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -7,19 +7,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Pusher = Pusher;
 
-const reverbScheme = import.meta.env.VITE_REVERB_SCHEME || 'http';
-const reverbHost = import.meta.env.VITE_REVERB_HOST || window.location.hostname;
-const reverbPort = Number(import.meta.env.VITE_REVERB_PORT || (reverbScheme === 'https' ? 443 : 8080));
-
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-
-    wsHost: reverbHost,
-    wsPort: reverbPort,
-    wssPort: reverbPort,
-
-    forceTLS: reverbScheme === 'https',
-
-    enabledTransports: [reverbScheme === 'https' ? 'wss' : 'ws'],
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
 });
