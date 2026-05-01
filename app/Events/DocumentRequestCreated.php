c12d8200ad5166d23894cba47b88208dcf5ed8f4
johnmichael-jonatas <johnmichael.jonatas@microsource.com.ph>
2026-05-01 14:13:17 +0800

    Add logging to DocumentRequestCreated and DocumentRequestStatusUpdated events

--- app/Events/DocumentRequestCreated.php
+++ app/Events/DocumentRequestCreated.php
@@ -8,12 +8,12 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log; // <-- Add this
 
-class DocumentRequestCreated implements ShouldBroadcast
+class DocumentRequestCreated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
